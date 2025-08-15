f8c6ed3ab73c0579b58df96aa823de3fe35dc1d8
habartakh <bartakh.hajar@gmail.com>
2025-08-15 00:38:19 +0000

    Added action server call directly from test node

--- test/tortoisebot_waypoints_test_cases.py
+++ test/tortoisebot_waypoints_test_cases.py
@@ -1,29 +1,35 @@
 #! /usr/bin/env python3
 
 from tortoisebot_waypoints.tortoisebot_action_server import WaypointActionClass
+import actionlib
+from tortoisebot_waypoints.msg import WaypointActionAction, WaypointActionGoal, WaypointActionResult, WaypointActionFeedback
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Quaternion, Point
 from tf.transformations import euler_from_quaternion
 import rospy
 import rosunit
 import unittest
 import rostest
 import time
+import math
 
 PKG = 'tortoisebot_waypoints'
 NAME = 'waypoint_action_class_integration_test'
 
-goal_x = 0.0
-goal_y = 0.2
-goal_yaw = 1.57 
-precision_xy = 0.1
-precision_yaw = 0.3
 
 class TestWaypointActionClass(unittest.TestCase):
 
     def setUp(self):
 
         rospy.init_node('test_node')
+
+        self.goal = Point()
+        self.goal.x = rospy.get_param('~goal_x', 0.0)
+        self.goal.y = rospy.get_param('~goal_y', 0.0)
+        self.goal_yaw = rospy.get_param('~goal_yaw', 0.0)
+        self.precision_xy = rospy.get_param('~precision_xy', 0.1)
+        self.precision_yaw = rospy.get_param('~precision_yaw', 0.2)
+
         self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_callback)
         self.current_orientation = Quaternion()
         self.current_x = 0 
@@ -37,6 +43,15 @@ class TestWaypointActionClass(unittest.TestCase):
         self.init_y = 0 
         self.final_y = 0
 
+        # Call the action server directly from test script to avoid timing issues:
+        # The timeout introduced previously (7s)in test functions is chosen randomly
+        # And sometimes the tests start before the robot completes its trajectory
+        
+        self.client = actionlib.SimpleActionClient('tortoisebot_as', WaypointActionAction)
+        self.client.wait_for_server()
+        self.success = False
+
+
     def odom_callback(self, msg):
 
         self.current_orientation = msg.pose.pose.orientation
@@ -49,47 +64,53 @@ class TestWaypointActionClass(unittest.TestCase):
         (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
         return yaw
 
-    def test_correct_position(self):
-        print ("Current position:")
-        print(str(self.current_x) + " ; " + str(self.current_y))
-        time.sleep(7)
-        
-        msg = rospy.wait_for_message("/odom", Odometry, timeout=2)
+     # send goal to the server
+    def action_server_call(self):
 
-        self.final_x = msg.pose.pose.position.x 
-        self.final_y = msg.pose.pose.position.y
+        action_goal = WaypointActionGoal()
+        action_goal.position = self.goal
+        self.client.send_goal(action_goal)
+
+
+    def test_correct_position(self):
+        
+        self.action_server_call()
+        # wait until the goal is reached
+        self.success = self.client.wait_for_result(rospy.Duration(30.0))
+
+        # Make sure that the goal is reached
+        self.assertTrue(self.success)
+        
+        # Now we get the proper final values 
+        self.final_x = self.current_x 
+        self.final_y = self.current_y
         print("Final X:" , self.final_x)
         print("Final Y:" , self.final_y)
        
-        x_diff = self.final_x - self.init_x
-        y_diff = self.final_y - self.init_y
+
+        x_diff = self.final_x - self.goal.x
+        y_diff = self.final_y - self.goal.y
         print ("X difference : ", x_diff)
         print ("Y difference : ", y_diff)
 
-        x_min = goal_x - precision_xy
-        x_max = goal_x + precision_xy
+        dist_to_goal = math.sqrt(pow(x_diff, 2) +
+            pow(y_diff, 2))
+        print ("dist_to_goal : ", dist_to_goal)
 
-        y_min = goal_y - precision_xy
-        y_max = goal_y + precision_xy
-
-        self.assertTrue((x_min <= x_diff <= x_max and y_min <= y_diff <= y_max ), "Integration error. Position was not between the expected values.")
+        self.assertTrue((dist_to_goal <= self.precision_xy), "Error: Position was not between the expected values.")
 
     def test_correct_rotation(self):
         
         print ("Current Orientation:",self.current_orientation )
         time.sleep(7)
         
-        #rospy.wait_for_message("/odom", Odometry, timeout=2)
         self.final_yaw = self.euler_to_quaternion(self.current_orientation)
         print("Final Yaw:", self.final_yaw )
-        yaw_diff = self.final_yaw - self.init_yaw
+        
+        yaw_diff = self.final_yaw - self.goal_yaw
         print ("Yaw Diff:", yaw_diff)
 
-        yaw_min = goal_yaw - precision_yaw
-        yaw_max = goal_yaw + precision_yaw
-
-
-        self.assertTrue((yaw_min <= yaw_diff <= yaw_max), "Integration error. Rotation was not between the expected values.")
+        self.assertTrue((math.fabs(yaw_diff)<= self.precision_yaw), "Error: Rotation was not between the expected values.")
 
 if __name__ == '__main__':
     rostest.rosrun(PKG, NAME, TestWaypointActionClass)
